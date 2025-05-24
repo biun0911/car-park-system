bf1b03546de49a9ef861956d4bfd86140126ddf2
J271729 <J271729@tafe.wa.edu.au>
2025-05-24 18:04:12 +0800

    log cars entering and exiting to log.txt file

--- src/car_park.py
+++ src/car_park.py
@@ -1,13 +1,18 @@
 from sensor import Sensor
 from display import Display
+from pathlib import Path
+from datetime import datetime
+
 
 class CarPark:
-    def __init__(self, location, capacity, plates=None, sensors=None, displays=None):
+    def __init__(self, location, capacity, plates=None, sensors=None, displays=None, log_file=Path("log.txt")):
         self.location = location
         self.capacity = capacity
         self.plates = plates or [] # uses the first value if not None, otherwise uses the second value
         self.sensors = sensors or []
         self.displays = displays or []
+        self.log_file = log_file if isinstance(log_file, Path) else Path(log_file)
+        self.log_file.touch(exist_ok=True)
 
     def __string__(self):
         return f"Car Park at {self.location}, with {self.capacity} bays."
@@ -21,13 +26,19 @@ class CarPark:
         elif isinstance(component, Display):
             self.displays.append(component)
 
+    def _log_car_activity(self, plate, action):
+        with self.log_file.open("a") as f:
+            f.write(f"{plate} {action} at {datetime.now():%Y-%m-%d %H:%M:%S}\n")
+
     def add_car(self, plate):
         self.plates.append(plate)
         self.update_displays()
+        self._log_car_activity(plate, "entered")
 
     def remove_car(self, plate):
         self.plates.remove(plate)
         self.update_displays()
+        self._log_car_activity(plate, "removed")
 
     @property
     def available_bays(self):
